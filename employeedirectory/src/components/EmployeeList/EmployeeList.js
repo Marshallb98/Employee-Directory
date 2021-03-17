876f1ab5be047a90c6705ae68227ac41225dfb21
Marshallb98 <69322012+Marshallb98@users.noreply.github.com>
2021-03-16 23:38:09 -0500

    Added sorting

--- employeedirectory/src/components/EmployeeList/EmployeeList.js
+++ employeedirectory/src/components/EmployeeList/EmployeeList.js
@@ -3,33 +3,95 @@ import API from "../../utils/API";
 import "./style.css";
 import Body from "../body/Body";
 import Jumbotron from "react-bootstrap/Jumbotron";
+
 class EmployeeList extends Component {
   state = {
     employees: [],
-    sortState: ""
+    sortState: [],
+    search: "",
   };
   componentDidMount() {
     this.loadEmployees();
   }
 
+  searchByTarget = (event) => {
+    this.setState({ search: event.target.value }, () => {
+      this.setState({
+        sortState: this.state.employees.filter((emp) =>
+          emp.name.first.toLowerCase().includes(this.state.search.toLowerCase())
+        ),
+      });
+    });
+  };
+
   loadEmployees = () => {
     API.getEmployees()
-      .then((res) => this.setState({ employees: res.data.results }))
+      .then((res) =>
+        this.setState({
+          employees: res.data.results,
+          sortState: res.data.results,
+        })
+      )
       .catch((err) => console.log(err));
   };
 
-  sortByFirst = () => {
-      
+  sortByFirst() {
+    function compare(a, b) {
+      const aName = a.name.first.toLowerCase();
+      const bName = b.name.first.toLowerCase();
+
+      let comparison = 0;
+      if (aName > bName) {
+        comparison = 1;
+      } else if (aName < bName) {
+        comparison = -1;
+      }
+
+      return comparison;
+    }
+
+    let sortedArray = this.state.sortState.sort(compare);
+    this.setState({ sortState: sortedArray });
   }
+
+  sortByLast() {
+    function compare(a, b) {
+      const aName = a.name.first.toLowerCase();
+      const bName = b.name.first.toLowerCase();
+
+      let comparison = 0;
+      if (aName < bName) {
+        comparison = 1;
+      } else if (aName > bName) {
+        comparison = -1;
+      }
+
+      return comparison;
+    }
+
+    let sortedArray = this.state.sortState.sort(compare);
+    this.setState({ sortState: sortedArray });
+  }
+
   render() {
     return (
       <div>
-        <Jumbotron >
+        <Jumbotron>
           <h1>Employee Directory</h1>
         </Jumbotron>
-        <div>
-          <Body employees={this.state.employees} />
-        </div>
+        <input
+          type="text"
+          value={this.state.search}
+          placeholder="Search By First Name"
+          onChange={this.searchByTarget}
+        ></input>
+        <button onClick={this.sortByFirst.bind(this)}>
+          First Name Alphabetical
+        </button>
+        <button onClick={this.sortByLast.bind(this)}>
+          First Name Non Alphabetical
+        </button>
+        <Body employees={this.state.sortState} />
       </div>
     );
   }
--- employeedirectory/src/components/body/Body.js
+++ employeedirectory/src/components/body/Body.js
@@ -2,37 +2,41 @@ import React from "react";
 import Table from "react-bootstrap/Table";
 import "./style.css";
 function Body(props) {
+  
   return (
-    <div>
       <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Employee</th>
-            <th>Years with Company</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Phone Number</th>
-          </tr>
-        </thead>
+         <thead>
+            <tr>
+              <th>Employee</th>
+              <th>Years with Company</th>
+              <th>
+                  First Name
+              </th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone Number</th>
+            </tr>
+          </thead>
         <tbody>
-          {props.employees.map(({ picture, registered, name, email, phone }) => {
-            return (
-              <tr>
-                <td>
-                  <img src={picture.medium} alt="IdPicture" /> 
-                </td>
-                <td>{registered.age}</td>
-                <td>{name.first}</td>
-                <td>{name.last}</td>
-                <td>{email}</td>
-                <td>{phone}</td>
-              </tr>
-            );
-          })}
+          {props.employees.map(
+            ({  login, picture, registered, name, email, phone }) => {
+              return (
+                
+                <tr key={login.uuid}>
+                  <td>
+                    <img src={picture.medium} alt="IdPicture" />
+                  </td>
+                  <td>{registered.age}</td>
+                  <td>{name.first}</td>
+                  <td>{name.last}</td>
+                  <td>{email}</td>
+                  <td>{phone}</td>
+                </tr>
+              );
+            }
+          )}
         </tbody>
       </Table>
-    </div>
   );
 }
 
